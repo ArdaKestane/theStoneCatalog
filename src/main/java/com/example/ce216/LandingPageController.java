81d3612735baedb0dbb6c7bdfcfa5e1f0ee515de
ArdaKestane <NL2rbc4aSMwydfu>
2022-05-30 01:58:18 +0300

    Added the Transitions and Buttons

---
+++ src/main/java/com/example/ce216/LandingPageController.java
@@ -0,0 +1,84 @@
+package com.example.ce216;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LandingPageController  {
+
+    @FXML
+    private VBox root;
+
+    @FXML
+    private StackPane stackPane;
+
+    @FXML
+    private VBox vBox;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Button button2;
+
+
+    @FXML
+    private Pane pane1;
+
+    @FXML
+    private Pane pane2;
+
+    @FXML
+    private Button add;
+
+    @FXML
+    private Button edit;
+
+    @FXML
+    private Button delete;
+
+    @FXML
+    private Button search;
+
+    @FXML
+    public void starter(){
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(2),vBox);
+        translateTransition.setToX(-root.getPrefWidth()/2+vBox.getPrefWidth()/2);
+        translateTransition.play();
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1.5),stackPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        button.setVisible(false);
+        button2.setVisible(false);
+
+        add.setVisible(true);
+        edit.setVisible(true);
+        delete.setVisible(true);
+        search.setVisible(true);
+
+ }
+
+ @FXML
+    public void nextPage(){
+        pane1.setVisible(false);
+        pane2.setVisible(true);
+ }
+
+ @FXML
+    public void previousPage(){
+        pane2.setVisible(false);
+        pane1.setVisible(true);
+ }
+}
--- src/main/java/com/example/ce216/Main.java
+++ src/main/java/com/example/ce216/Main.java
@@ -2,15 +2,17 @@ package com.example.ce216;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LandingPage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LandingPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Catalog");
         stage.setScene(scene);
